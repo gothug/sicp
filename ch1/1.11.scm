dec0d288b5e508f9aaa3234b1970da3b1bce6aa7
gothug <kojuhovskiy@gmail.com>
2014-08-01 12:02:51 +0400

    1.11 excercise

---
+++ ch1/1.11.scm
@@ -0,0 +1,20 @@
+; recursive
+(define (f n)
+  (cond ((< n 3) n)
+        (else (+ (f (- n 1))
+                 (* 2 (f (- n 2)))
+                 (* 3 (f (- n 3))))
+        )))
+
+; f(n) = (n < 2 n, f(n-1) + 2 * f(n-2))
+; a = 1
+; b = 0
+
+; iterative
+(define (fi n)
+  (f-iter 2 1 0 n))
+
+(define (f-iter a b c count)
+  (if (= count 0)
+      c
+      (f-iter (+ a (* 2 b) (* 3 c)) a b (- count 1))))
