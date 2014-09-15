a793626d1fda6ad602a4a8739ca5be662a7c944c
gothug <kojuhovskiy@gmail.com>
2014-09-15 14:47:22 +0200

    ex 2.19

---
+++ ch2/2.19.scm
@@ -0,0 +1,31 @@
+; Solution
+(define us-coins (list 50 25 10 5 1))
+(define uk-coins (list 100 50 20 10 5 2 1 0.5))
+
+(define (cc amount coin-values)
+  (cond ((= amount 0) 1)
+        ((or (< amount 0) (no-more? coin-values)) 0)
+        (else
+         (+ (cc amount
+                (except-first-denomination
+                 coin-values))
+            (cc (- amount
+                   (first-denomination
+                    coin-values))
+                coin-values)))))
+
+(define (first-denomination coin-values)
+  (car coin-values))
+
+(define (no-more? coin-values)
+  (null? coin-values))
+
+(define (except-first-denomination coin-values)
+  (cdr coin-values))
+
+; Tests
+(newline)
+(display "Running tests:")(newline)
+(display "==============")(newline)
+(display "292  = ")(display (cc 100 us-coins))(newline)
+(display "6149 = ")(display (cc 50 uk-coins))(newline)
