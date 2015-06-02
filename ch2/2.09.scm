ab052aa0e6aa533ea6566977d39ae829c44716bf
gothug <kojuhovskiy@gmail.com>
2015-06-02 23:06:03 +0300

    ex 2.09

---
+++ ch2/2.09.scm
@@ -0,0 +1,21 @@
+; Solution
+N1 = n1 +/- w1
+N2 = n2 +/- w2
+
+Addition:
+---
+
+N1 + N2 = (n1 - w1) + (n2 - w2) .. (n1 + w1) + (n2 + w2) =
+  (n1 + n2) +/- (w1 + w2)
+
+=> width(N1 + N2) = width(N1) + width(N2)
+
+Subtraction:
+---
+
+N1 - N2 = (n1 - w1) - (n2 + w2) .. (n1 + w1) - (n2 - w2) =
+  (n1 - n2) +/- (w1 + w2)
+
+=> width(N1 - N2) = width(N1) + width(N2)
+
+Multiplication:
