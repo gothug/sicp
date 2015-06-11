235b4eb3485db2159c684c357fd0633a5b4d0838
gothug <kojuhovskiy@gmail.com>
2015-06-11 20:33:37 +0300

    funprog.scm

---
+++ lib/funprog.scm
@@ -0,0 +1,8 @@
+(define (accumulate op initial sequence)
+  (if (null? sequence)
+      initial
+      (op (car sequence)
+          (accumulate op initial (cdr sequence)))))
+
+(define (flatmap proc seq)
+  (accumulate append (list) (map proc seq)))
