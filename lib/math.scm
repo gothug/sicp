10b52baa452b7f54dafbb0c4613c8de696728ca6
gothug <kojuhovskiy@gmail.com>
2015-06-11 20:33:17 +0300

    math.scm

--- lib/math.scm
+++ lib/math.scm
@@ -9,3 +9,8 @@
 
 (define (prime? n)
   (= n (smallest-divisor n)))
+
+(define (enumerate-interval low high)
+  (if (> low high)
+      (list)
+      (cons low (enumerate-interval (+ low 1) high))))
