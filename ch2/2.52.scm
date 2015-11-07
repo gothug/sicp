6499fb4bae237f45c644bc2d8c564742ca30747a
gothug <kojuhovskiy@gmail.com>
2015-11-07 14:00:54 +0300

    ex 2.52

---
+++ ch2/2.52.scm
@@ -0,0 +1,20 @@
+; Solution
+
+; b.
+(define (corner-split painter n)
+  (if (= n 0)
+      painter
+      (let ((up (up-split painter (- n 1)))
+            (right (right-split painter (- n 1))))
+        (let ((top-left up)
+              (bottom-right right)
+              (corner (corner-split painter (-n 1))))
+          (beside (below painter top-left)
+                  (below bottom-right corner))))))
+
+; c.
+(define (square-limit painter n)
+  (let ((combine4 (square-of-four identity flip-horiz
+                                  flip-vert rotate180)))
+    (combine4 (corner-split painter n))))
+
