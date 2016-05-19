bee4f87830520931c35ef275a53393d7d1cb835f
gothug <kojuhovskiy@gmail.com>
2016-05-19 21:49:50 +0300

    ex 3.56

---
+++ ch3/3.56.scm
@@ -0,0 +1,8 @@
+; Solution
+(define S
+  (cons-stream 1
+               (merge
+                (scale-stream S 2)
+                (merge
+                 (scale-stream S 3)
+                 (scale-stream S 5)))))
