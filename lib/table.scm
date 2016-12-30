ef169d41038f59463b440b150eb98fff37fc38eb
gothug <kojuhovskiy@gmail.com>
2016-12-30 14:56:44 +0300

    lib/table.scm

---
+++ lib/table.scm
@@ -0,0 +1,21 @@
+(load "../lib/util.scm")
+
+(define table '())
+
+(define (put key value)
+  (set! table (cons (list key value) table)))
+
+(define (get key)
+  (define (search key t)
+    (cond
+     ((null? t) #f)
+     ((eqv? (caar t) key) (cadar t))
+     (else (search key (cdr t)))))
+  (search key table))
+
+; Tests
+(run-tests-header)
+(put 'key1 1)
+
+(should-be (get 'key1) 1)
+(should-be (get 'key2) #f)
