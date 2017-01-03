cc710c23b693c29a517637372bbf6dd612dde814
gothug <kojuhovskiy@gmail.com>
2017-01-03 15:38:03 +0300

    lib/interpreter.scm

--- lib/interpreter.scm
+++ lib/interpreter.scm
@@ -4,6 +4,12 @@
       (eq? (car exp) tag)
       false))
 
+; predicates
+(define (true? x)
+  (not (eq? x false)))
+(define (false? x)
+  (eq? x false))
+
 ; self-evaluating
 (define (self-evaluating? exp)
   (cond ((number? exp) true)
@@ -54,6 +60,74 @@
 (define (make-lambda parameters body)
   (cons 'lambda (cons parameters body)))
 
+; if
+(define (if? exp) (tagged-list? exp 'if))
+(define (if-predicate exp) (cadr exp))
+(define (if-consequent exp) (caddr exp))
+(define (if-alternative exp)
+  (if (not (null? (cdddr exp)))
+      (cadddr exp)
+      'false))
+
+(define (make-if predicate consequent alternative)
+  (list 'if predicate consequent alternative))
+
+(define (eval-if exp env)
+  (if (true? (eval (if-predicate exp) env))
+      (eval (if-consequent exp) env)
+      (eval (if-alternative exp) env)))
+
+; begin
+(define (begin? exp) (tagged-list? exp 'begin))
+(define (begin-actions exp) (cdr exp))
+(define (last-exp? seq) (null? (cdr seq)))
+(define (first-exp seq) (car seq))
+(define (rest-exps seq) (cdr seq))
+
+(define (sequence->exp seq)
+  (cond ((null? seq) seq)
+        ((last-exp? seq) (first-exp seq))
+        (else (make-begin seq))))
+(define (make-begin seq) (cons 'begin seq))
+
+(define (eval-sequence exps env)
+  (cond ((last-exp? exps) (eval (first-exp exps) env))
+        (else (eval (first-exp exps) env)
+              (eval-sequence (rest-exps exps) env))))
+
+; cond
+(define (cond? exp) (tagged-list? exp 'cond))
+(define (cond-clauses exp) (cdr exp))
+(define (cond-else-clause? clause)
+  (eq? (cond-predicate clause) 'else))
+(define (cond-predicate clause) (car clause))
+(define (cond-actions clause) (cdr clause))
+
+(define (cond->if exp)
+  (expand-clauses (cond-clauses exp)))
+
+(define (cond-special-syntax? clause) (eq? (cadr clause) '=>))
+(define (cond-special-syntax-predicate clause) (car clause))
+(define (cond-special-syntax-recipient clause) (caddr clause))
+
+(define (expand-clauses clauses)
+  (if (null? clauses)
+      'false                          ; no else clause
+      (let ((first (car clauses))
+            (rest (cdr clauses)))
+        (if (cond-else-clause? first)
+            (if (null? rest)
+                (sequence->exp (cond-actions first))
+                (error "ELSE clause isn't last -- COND->IF"
+                       clauses))
+            (if (cond-special-form-clause? first)
+                (make-if (cond-special-syntax-predicate first)
+                         (list (cond-special-syntax-recipient first) (cond-special-syntax-predicate first))
+                         (expand-clauses rest))
+                (make-if (cond-predicate first)
+                         (sequence->exp (cond-actions first))
+                         (expand-clauses rest)))))))
+
 ; application
 (define (application? exp) (pair? exp))
 (define (operator exp) (car exp))
@@ -73,14 +147,14 @@
         ((quoted? exp) (text-of-quotation exp))
         ((assignment? exp) (eval-assignment exp env))
         ((definition? exp) (eval-definition exp env))
-        ;((if? exp) (eval-if exp env))
+        ((if? exp) (eval-if exp env))
         ;((lambda? exp)
         ; (make-procedure (lambda-parameters exp)
         ;                 (lambda-body exp)
         ;                 env))
-        ;((begin? exp) 
-        ; (eval-sequence (begin-actions exp) env))
-        ;((cond? exp) (eval (cond->if exp) env))
+        ((begin? exp)
+         (eval-sequence (begin-actions exp) env))
+        ((cond? exp) (eval (cond->if exp) env))
         (else
          (let ((eval-proc (get (car exp))))
            (if eval-proc
