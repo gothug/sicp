64f32341904e2fe90bd76ea0aacdbaa985ed7e68
gothug <kojuhovskiy@gmail.com>
2017-01-03 15:39:15 +0300

    ex 4.05

---
+++ ch4/4.05.scm
@@ -0,0 +1,22 @@
+(load "../lib/interpreter.scm")
+(load "../lib/util.scm")
+(load "../lib/table.scm")
+
+; Solution
+(define (apply procedure arguments) (cons "applying" (cons procedure arguments)))          ; dummy apply
+; special syntax support for cond is implemented in file lib/interpreter.scm
+
+; Tests
+(run-tests-header)
+
+(define env '())
+
+(test "ordinary cond")
+  (should-be (eval '(cond (#f 0) (#t 1) (else 2)) env) 1)
+  (should-be (eval '(cond (#f 0) (#f 1) (else 2)) env) 2)
+  (should-be (eval '(cond (else 0)) env) 0)
+
+(test "special form cond")
+  (should-be (eval '(cond (#f 0) (1 => "proc") (else 2)) env) '("applying" "proc" 1))
+  (should-be (eval '(cond (#f 0) (#f => "proc") (else 2)) env) 2)
+  (should-be (eval '(cond (#t 0) (1 => "proc") (else 2)) env) 0)
