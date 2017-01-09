6384736a98145cd1ede99c049247113c55b9414d
Vasily Kozhukhovskiy <v.kozhukhovskiy@digsolab.com>
2017-01-09 19:34:11 +0300

    ex 4.21

---
+++ ch4/4.21.scm
@@ -0,0 +1,29 @@
+(load "../lib/util.scm")
+
+; Solution
+
+; a
+(define fib
+  (lambda (n)
+   ((lambda (fib) (fib fib n))
+    (lambda (ft k)
+      (cond ((= k 0) 0)
+            ((= k 1) 1)
+            (else (+ (ft ft (- k 2)) (ft ft (- k 1)))))))))
+
+; b
+(define (f x)
+  ((lambda (even? odd?)
+     (even? even? odd? x))
+   (lambda (ev? od? n)
+     (if (= n 0) true (od? ev? od? (- n 1))))
+   (lambda (ev? od? n)
+     (if (= n 0) false (ev? ev? od? (- n 1))))))
+
+; Tests
+(run-tests-header)
+
+(define the-global-environment (setup-environment))
+
+(test "fib")
+  (should-be (fib 10) 55)
